a53418fcd66e8707c24baa01c8093d7f77d688d6
Domenico Pagano <dpaganoh@gmail.com>
2024-07-16 22:57:54 -0500

    webpack

--- build/bundle.js
+++ build/bundle.js
@@ -15,7 +15,7 @@
   \*************************/
 /***/ (() => {
 
-eval("alert('Welcome');\n\n//# sourceURL=webpack://family_values/./src/js/index.js?");
+eval("const imagenes = document.querySelectorAll('.galeriaPage__imagen');\r\nconst fullscreen = document.getElementById('fullscreen');\r\n\r\nimagenes.forEach(img => {\r\n    img.addEventListener('click', abrirImagen);\r\n})\r\n\r\nfunction abrirImagen(e){\r\n    const url = e.target.src\r\n\r\n    let screen = document.querySelector('.fullscreen__absoluto');\r\n    if(!screen) {\r\n        screen = document.createElement('div');\r\n        screen.classList.add('fullscreen__absoluto');\r\n        fullscreen.appendChild(screen);\r\n    }\r\n\r\n    const img = document.createElement('img');\r\n    img.classList.add('fullscreen__imagen');\r\n    img.src = url;\r\n\r\n    // Limpiar 'screen' y mostrar nueva imagen\r\n    screen.innerHTML = '';\r\n    screen.appendChild(img);\r\n\r\n    // Cerrar la imagen al hacer click fuera\r\n    screen.addEventListener('click', () => {\r\n        fullscreen.removeChild(screen);\r\n    })\r\n}\n\n//# sourceURL=webpack://family_values/./src/js/index.js?");
 
 /***/ })
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
-alert('Welcome');
+const imagenes = document.querySelectorAll('.galeriaPage__imagen');
+const fullscreen = document.getElementById('fullscreen');
+
+imagenes.forEach(img => {
+    img.addEventListener('click', abrirImagen);
+})
+
+function abrirImagen(e){
+    const url = e.target.src
+
+    let screen = document.querySelector('.fullscreen__absoluto');
+    if(!screen) {
+        screen = document.createElement('div');
+        screen.classList.add('fullscreen__absoluto');
+        fullscreen.appendChild(screen);
+    }
+
+    const img = document.createElement('img');
+    img.classList.add('fullscreen__imagen');
+    img.src = url;
+
+    // Limpiar 'screen' y mostrar nueva imagen
+    screen.innerHTML = '';
+    screen.appendChild(img);
+
+    // Cerrar la imagen al hacer click fuera
+    screen.addEventListener('click', () => {
+        fullscreen.removeChild(screen);
+    })
+}
